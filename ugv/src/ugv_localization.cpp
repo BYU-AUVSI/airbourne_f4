3bf808484986abd7e6f1b2ba9c802d63ade42e7f
Connor <connorolsen72@gmail.com>
2019-06-04 19:53:32 -0600

    added ugv_localization class

--- ugv/src/ugv_localization.cpp
+++ ugv/src/ugv_localization.cpp
@@ -3,3 +3,39 @@
  * Author: Jacob Willis
  * Date Started: 30-May-2019
  */
+
+#include "ugv_localization.h"
+
+void UGV_LOCALIZATION::init(int uart_num){
+  systemInit();
+
+  vcp.init();
+  serPtr = &vcp;
+
+  uart.init(&uart_config[UART3], 115200);
+
+  init_printf(NULL, _putc);
+
+  gps.init(&uart);
+
+  led1.init(LED1_GPIO, LED1_PIN);
+
+}
+
+void UGV_LOCALIZATION::pull_gps(float *lat, float *lon){
+  while (1)
+  {
+    if (gps.new_data())
+    {
+      struct UBLOX::GNSSPVT data;
+    data = gps.read();
+    printf("t: %d\tlla: %6.6f, %6.6f, %4.2f\tvel: %3.3f, %3.3f, %3.3f\n\r",
+       data.time, (float)data.lat, (float)data.lon, (float)data.height,
+       (double)data.vel_n, (double)data.vel_e, (double)data.vel_d);
+      led1.toggle();
+      *lat = (float)data.lat;
+      *lon = (float)data.lon;
+      return;
+    }
+  }
+}
--- ugv/src/ugv_localization.h
+++ ugv/src/ugv_localization.h
@@ -8,19 +8,32 @@
 #ifndef UGV_LOCALIZ_H
 #define UGV_LOCALIZ_H
 
+#include "serial.h"
+#include "system.h"
+#include "uart.h"
+#include "vcp.h"
 #include "ublox.h"
+#include "revo_f4.h"
+#include "printf.h"
+#include "led.h"
 
-class UGV_LOCALIZATION 
+class UGV_LOCALIZATION
 {
 	public:
 
-		void init(UBLOX gnss);
-		
+		void init(int uart_num);
+
+		void pull_gps(float *lat, float *lon);
 
 	private:
-		UBLOX _ubx
-		PWM_OUT servo_pwm;
-
+		VCP vcp;
+		UART uart;
+		UBLOX gps;
+		LED led1;
+		double lla[3] = {};
+	  float vel[3] = {};
+	  uint8_t fix_type = 0;
+	  uint32_t t_ms;
 };
 
 #endif /* UGV_LOCALIZ_H */
